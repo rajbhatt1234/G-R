714274093b64e78fa97c89988bd998cae24551cc
rajbhatt1234 <85678820+rajbhatt1234@users.noreply.github.com>
2024-12-15 07:44:41 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import './styles.css';
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <h1>Hey Love 💕</h1>
+        <p>Thinking about you makes my day brighter, and I can't wait to see you
+        again. Until then, here’s a little virtual hug to keep you company.
+        </p>
+      </header>
+      <main className="main-content">
+        <div className="message-box">
+          <h2>Tara vagar gamtu nathi mne</h2>
+          <p>
+          I JUST WANTED TO SAY THAT I MISS YOU VERY MUCH AND I DONT LIKE THAT YOU ARE NOT WITH ME I VALUE SO YOU SO MUCH THANK YOU SO MUCH FOR MAKEING MY LIFE THIS AMAZING.
+          I REALLY LOVE YOU A LOT MARU GRIVUDU
+          </p>
+          <p className="hug">🤗</p>
+        </div>
+
+        <div className="heart-animation">
+          <div className="heart"></div>
+          <div className="heart"></div>
+          <div className="heart"></div>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <p>
+          Made with 💖 by me Raj OFC .
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
